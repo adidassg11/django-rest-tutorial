4a9f2bac68255bb53c88ce42e340e0a17e25e989
Sal Gomez <sagomez@microsoft.com>
2019-08-12 16:45:41 -0700

    pagination

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -72,6 +72,12 @@ TEMPLATES = [
 WSGI_APPLICATION = 'tutorial.wsgi.application'
 
 
+# API PAGINATION
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 2
+}
+
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
