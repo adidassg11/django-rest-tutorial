4126b86055ac42316c71a209084038d9d1072aa2
Sal Gomez <sagomez@microsoft.com>
2019-08-12 13:11:13 -0700

    fixed bux, now showing details

--- snippets/views.py
+++ snippets/views.py
@@ -1,55 +1,32 @@
-# from django.http import Http404
-# from rest_framework import status
-# from rest_framework.views import APIView
-# from rest_framework.response import Response
-#
-# from snippets.models import Snippet
-# from snippets.serializers import SnippetSerializer
+from django.http import Http404
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
-from django.http import Http404
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
 
-
-# class SnippetList(APIView):
-#     """
-#     List all code snippets, or create a new snippet.
-#     """
-#     # if request.method == 'GET':
-#     def get(self, request, format=None):
-#         snippets = Snippet.objects.all()
-#         serializer = SnippetSerializer(snippets, many=True)
-#         # return JsonResponse(serializer.data, safe=False)  # Old django style
-#         return Response(serializer.data)
-#
-#     #elif request.method == 'POST':
-#     def post(self, request, format=None):
-#         # data = JSONParser().parse(request)  # Old Django style
-#         serializer = SnippetSerializer(data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             # return JsonResponse(serializer.data, status=201)  # Old django style
-#             return Response(serializer.data, status=status.HTTP_201_CREATED)
-#         # return JsonResponse(serializer.errors, status=400)  # Old Django style
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class SnippetList(APIView):
     """
-    List all snippets, or create a new snippet.
+    List all code snippets, or create a new snippet.
     """
+    # if request.method == 'GET':
     def get(self, request, format=None):
         snippets = Snippet.objects.all()
         serializer = SnippetSerializer(snippets, many=True)
+        # return JsonResponse(serializer.data, safe=False)  # Old django style
         return Response(serializer.data)
 
+    #elif request.method == 'POST':
     def post(self, request, format=None):
+        # data = JSONParser().parse(request)  # Old Django style
         serializer = SnippetSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
+            # return JsonResponse(serializer.data, status=201)  # Old django style
             return Response(serializer.data, status=status.HTTP_201_CREATED)
+        # return JsonResponse(serializer.errors, status=400)  # Old Django style
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
@@ -61,7 +38,7 @@ class SnippetDetail(APIView):
     """
     def get_object(self, pk):
         try:
-            snippet = Snippet.objects.get(pk=pk)
+            return Snippet.objects.get(pk=pk)
         except Snippet.DoesNotExist:
             # return Response(status=status.HTTP_404_NOT_FOUND)
             raise Http404
